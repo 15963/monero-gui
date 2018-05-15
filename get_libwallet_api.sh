b981e6476706b55c7b5567fa57ab328eefab22dd
fu <515070577@qq.com>
2018-05-15 14:27:09 +0800

    update rrnc git url

--- get_libwallet_api.sh
+++ get_libwallet_api.sh
@@ -17,7 +17,7 @@ if [ ! -d $MONERO_DIR/src ]; then
 fi
 git submodule update --remote
 git -C $MONERO_DIR fetch
-git -C $MONERO_DIR checkout etn-v0.11.1.0-xmrig-exe-rpc
+git -C $MONERO_DIR checkout etn-v0.11.1.0-bh2-rpc
 
 # get monero core tag
 # get_tag
